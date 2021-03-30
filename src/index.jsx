31830b7cabae59fd53ff474543ace219d5ff3a6c
angel925 <johan.angel.925@gmail.com>
2021-03-30 17:19:51 -0500

    Primeros pasos con React

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // REACT DOM PARA QUE CORRA EN EL NAVEGADOR
+//importar el conponte
+import App from './App'
+
+
+
+//utilizar el componente  
+ReactDOM.render(<App />, 
+  document.getElementById('root'));
